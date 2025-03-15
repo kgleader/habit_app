c09695ce18d0925e31814a036da5975d0054d965
unknown <44379294+kgleader@users.noreply.github.com>
2025-03-14 21:21:15 -0600

    new app

---
+++ lib/models/habit.dart
@@ -0,0 +1,16 @@
+import 'package:isar/isar.dart';
+//run cmd to generate file: dart run build_runner build
+part 'habit.g.dart';
+
+@Collection()
+class Habit {
+  // habit id
+  Id id = Isar.autoIncrement;
+  // habit name
+  late String name;
+  // completed days
+  List<DateTime> completedDays = [
+    // DateTime(year, month, day),
+    // DateTime(2025, 3, 15),
+  ];
+}
